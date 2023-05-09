e0fbe6b46ddfadf2048bf6cb297a268e7855c58a
Benjamin <Benjamin.valleix@gmail.com>
2023-05-09 22:52:18 +0200

    :zap: Ajout vue Bibliothèque avec premier item

---
+++ PodcastApp/PodcastApp/PodcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastAppApp.swift
+//  PodcastApp
+//
+//  Created by etudiant on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PodcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
